922c1da8ab7b51f66e3a8b29b698ea3241c47939
Thiago Lopes Costa <lliwlopes@gmail.com>
2021-02-11 11:34:33 -0400

    1a Submissão
    Submissão da Aula 01 de IPC para teste

---
+++ public/js/editareaaula01.js
@@ -0,0 +1,53 @@
+editAreaLoader.init({
+	id: "tpc03_08_ta01"	// id of the textarea to transform		
+	,start_highlight: true	// if start with highlight
+	,allow_resize: "none"
+	,allow_toggle: false
+	,toolbar: "search, go_to_line, |, undo, redo, |, select_font"
+	,word_wrap: true
+	,language: "en"
+	,syntax: "Python"	
+});
+editAreaLoader.init({
+	id: "tpc03_09_ta01"	// id of the textarea to transform		
+	,start_highlight: true	// if start with highlight
+	,allow_resize: "none"
+	,allow_toggle: false
+	,is_editable: false
+	,word_wrap: true
+	,language: "en"
+	,syntax: "Python"
+});
+editAreaLoader.init({
+	id: "tpc03_09_ta02"	// id of the textarea to transform		
+	,start_highlight: true	// if start with highlight
+	,allow_resize: "none"
+	,allow_toggle: false
+	,is_editable: false
+	,word_wrap: true
+	,language: "en"
+	,syntax: "Python"
+});
+/*/ editArea para inserção de Códigos
+//Recebe o código
+editAreaLoader.init({
+	id: "tpc0?_??_ta01"	// id of the textarea to transform		
+	,start_highlight: false	// if start with highlight
+	,allow_resize: "none"
+	,allow_toggle: false
+	,toolbar: "search, go_to_line, |, undo, redo, |, select_font"
+	,word_wrap: true
+	,language: "en"
+	,syntax: "Python"	
+});
+//Mostra o código
+editAreaLoader.init({
+	id: "tpc0?_??_ta??"	// id of the textarea to transform		
+	,start_highlight: false	// if start with highlight
+	,allow_resize: "none"
+	,allow_toggle: false
+	,is_editable: false
+	,word_wrap: true
+	,language: "en"
+	,syntax: "Python"
+});*/	
