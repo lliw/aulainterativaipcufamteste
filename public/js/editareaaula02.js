6933e8d552f460d2c5ff00ea65b0502193f24f94
Thiago Lopes Costa <lliwlopes@gmail.com>
2021-04-22 09:30:43 -0400

    18a Submissão
    Inserção da aula 02

---
+++ public/js/editareaaula02.js
@@ -0,0 +1,236 @@
+editAreaLoader.init({
+    id: "tpc01_12_ta01"	// id of the textarea to transform		
+    ,start_highlight: true	// if start with highlight
+    ,allow_resize: "none"
+    ,allow_toggle: false
+    ,toolbar: "search, go_to_line, |, undo, redo, |, select_font"
+    ,word_wrap: true
+    ,language: "en"
+    ,syntax: "python"	
+});
+
+editAreaLoader.init({
+    id: "tpc01_12_01_ta01"	// id of the textarea to transform		
+    ,start_highlight: true	// if start with highlight
+    ,allow_resize: "none"
+    ,allow_toggle: false
+    ,is_editable: false
+    ,word_wrap: true
+    ,language: "en"
+    ,syntax: "python"
+});
+
+editAreaLoader.init({
+    id: "tpc01_12_01_ta02"	// id of the textarea to transform		
+    ,start_highlight: true	// if start with highlight
+    ,allow_resize: "none"
+    ,allow_toggle: false
+    ,is_editable: false
+    ,word_wrap: true
+    ,language: "en"
+    ,syntax: "python"
+});
+
+//exemplo 2
+editAreaLoader.init({
+    id: "tpc01_13_ta01"	// id of the textarea to transform		
+    ,start_highlight: true	// if start with highlight
+    ,allow_resize: "none"
+    ,allow_toggle: false
+    ,toolbar: "search, go_to_line, |, undo, redo, |, select_font"
+    ,word_wrap: false
+    ,language: "en"
+    ,syntax: "python"	
+});
+
+editAreaLoader.init({
+    id: "tpc01_13_01_ta01"	// id of the textarea to transform		
+    ,start_highlight: true	// if start with highlight
+    ,allow_resize: "none"
+    ,allow_toggle: false
+    ,is_editable: false
+    ,word_wrap: false
+    ,language: "en"
+    ,syntax: "python"
+});
+
+editAreaLoader.init({
+    id: "tpc01_13_01_ta02"	// id of the textarea to transform		
+    ,start_highlight: true	// if start with highlight
+    ,allow_resize: "none"
+    ,allow_toggle: false
+    ,is_editable: false
+    ,word_wrap: false
+    ,language: "en"
+    ,syntax: "python"
+});
+
+//problema 1
+editAreaLoader.init({
+    id: "tpc03_01_01_ta01"	// id of the textarea to transform		
+    ,start_highlight: true	// if start with highlight
+    ,allow_resize: "none"
+    ,allow_toggle: false
+    ,toolbar: "search, go_to_line, |, undo, redo, |, select_font"
+    ,word_wrap: true
+    ,language: "en"
+    ,syntax: "python"	
+})
+
+editAreaLoader.init({
+    id: "tpc03_01_02_ta01"	// id of the textarea to transform		
+    ,start_highlight: true	// if start with highlight
+    ,allow_resize: "none"
+    ,allow_toggle: false
+    ,is_editable: false
+    ,word_wrap: true
+    ,language: "en"
+    ,syntax: "python"
+});
+
+editAreaLoader.init({
+    id: "tpc03_01_02_ta02"	// id of the textarea to transform		
+    ,start_highlight: true	// if start with highlight
+    ,allow_resize: "none"
+    ,allow_toggle: false
+    ,is_editable: false
+    ,word_wrap: true
+    ,language: "en"
+    ,syntax: "python"
+});
+
+//problema 2
+editAreaLoader.init({
+    id: "tpc03_02_01_ta01"	// id of the textarea to transform		
+    ,start_highlight: true	// if start with highlight
+    ,allow_resize: "none"
+    ,allow_toggle: false
+    ,toolbar: "search, go_to_line, |, undo, redo, |, select_font"
+    ,word_wrap: true
+    ,language: "en"
+    ,syntax: "python"	
+})
+
+editAreaLoader.init({
+    id: "tpc03_02_02_ta01"	// id of the textarea to transform		
+    ,start_highlight: true	// if start with highlight
+    ,allow_resize: "none"
+    ,allow_toggle: false
+    ,is_editable: false
+    ,word_wrap: true
+    ,language: "en"
+    ,syntax: "python"
+});
+
+editAreaLoader.init({
+    id: "tpc03_02_02_ta02"	// id of the textarea to transform		
+    ,start_highlight: true	// if start with highlight
+    ,allow_resize: "none"
+    ,allow_toggle: false
+    ,is_editable: false
+    ,word_wrap: true
+    ,language: "en"
+    ,syntax: "python"
+});
+
+//problema 3
+editAreaLoader.init({
+    id: "tpc03_03_01_ta01"	// id of the textarea to transform		
+    ,start_highlight: true	// if start with highlight
+    ,allow_resize: "none"
+    ,allow_toggle: false
+    ,toolbar: "search, go_to_line, |, undo, redo, |, select_font"
+    ,word_wrap: true
+    ,language: "en"
+    ,syntax: "python"	
+})
+
+editAreaLoader.init({
+    id: "tpc03_03_02_ta01"	// id of the textarea to transform		
+    ,start_highlight: true	// if start with highlight
+    ,allow_resize: "none"
+    ,allow_toggle: false
+    ,is_editable: false
+    ,word_wrap: true
+    ,language: "en"
+    ,syntax: "python"
+});
+
+editAreaLoader.init({
+    id: "tpc03_03_02_ta02"	// id of the textarea to transform		
+    ,start_highlight: true	// if start with highlight
+    ,allow_resize: "none"
+    ,allow_toggle: false
+    ,is_editable: false
+    ,word_wrap: true
+    ,language: "en"
+    ,syntax: "python"
+});
+
+//problema 4
+editAreaLoader.init({
+    id: "tpc03_04_01_ta01"	// id of the textarea to transform		
+    ,start_highlight: true	// if start with highlight
+    ,allow_resize: "none"
+    ,allow_toggle: false
+    ,toolbar: "search, go_to_line, |, undo, redo, |, select_font"
+    ,word_wrap: true
+    ,language: "en"
+    ,syntax: "python"	
+})
+
+editAreaLoader.init({
+    id: "tpc03_04_02_ta01"	// id of the textarea to transform		
+    ,start_highlight: true	// if start with highlight
+    ,allow_resize: "none"
+    ,allow_toggle: false
+    ,is_editable: false
+    ,word_wrap: true
+    ,language: "en"
+    ,syntax: "python"
+});
+
+editAreaLoader.init({
+    id: "tpc03_04_02_ta02"	// id of the textarea to transform		
+    ,start_highlight: true	// if start with highlight
+    ,allow_resize: "none"
+    ,allow_toggle: false
+    ,is_editable: false
+    ,word_wrap: true
+    ,language: "en"
+    ,syntax: "python"
+});
+
+//problema 5
+editAreaLoader.init({
+    id: "tpc03_05_01_ta01"	// id of the textarea to transform		
+    ,start_highlight: true	// if start with highlight
+    ,allow_resize: "none"
+    ,allow_toggle: false
+    ,toolbar: "search, go_to_line, |, undo, redo, |, select_font"
+    ,word_wrap: true
+    ,language: "en"
+    ,syntax: "python"	
+})
+
+editAreaLoader.init({
+    id: "tpc03_05_02_ta01"	// id of the textarea to transform		
+    ,start_highlight: true	// if start with highlight
+    ,allow_resize: "none"
+    ,allow_toggle: false
+    ,is_editable: false
+    ,word_wrap: true
+    ,language: "en"
+    ,syntax: "python"
+});
+
+editAreaLoader.init({
+    id: "tpc03_05_02_ta02"	// id of the textarea to transform		
+    ,start_highlight: true	// if start with highlight
+    ,allow_resize: "none"
+    ,allow_toggle: false
+    ,is_editable: false
+    ,word_wrap: true
+    ,language: "en"
+    ,syntax: "python"
+});
