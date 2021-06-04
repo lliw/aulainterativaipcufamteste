4fb306204ff65e1dcc82e2ca22f2cb031078e82c
Thiago Lopes Costa <lliwlopes@gmail.com>
2021-06-04 14:20:07 -0400

    22 submissão
    Teste com arquivo ajax

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -1,11 +1,14 @@
 function loadDoc() {
+  var teste = "null";
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
      document.getElementById("demo").innerHTML = this.responseText;
-     //this.responseText = this.responseText+" Teste";
+     teste = this.responseText + "123 Teste";
     }
   };
   xhttp.open("GET", "ajax_info.txt", true);
   xhttp.send();
+  xhttp.open("POST", "ajax_info.txt", true);
+  xhttp.send(teste);
 }
