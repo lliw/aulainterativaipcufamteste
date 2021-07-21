0dcf23de90fe8b1ed8e37bde53114c7bd9a2e3c0
Thiago Lopes Costa <lliw.lopes@gmail.com>
2021-07-21 18:47:14 -0400

    TESTE AJAX 16

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -3,15 +3,16 @@
 function loadDoc() {
   var xhr = new XMLHttpRequest(),
   path = "public/js/ajax_path.php";
-
+  txt,
   xhr.open("GET", path, true);
   xhr.onreadystatechange = function()
   {
     if(xhr.readyState == 4 && xhr.status == 200)
     {
-        document.getElementById("demo").innerHTML = this.responseText + "Pronto";
+        txt = this.responseText;
+        document.getElementById("demo").innerHTML = txt + "Pronto";
         xhr.open("POST", path, true);
-        xhr.send("texto="+this.responseText+"a");
+        xhr.send("texto="+txt+"a");
         // but on this place you have to have a server for write updated JSON to the file
     }
   };
