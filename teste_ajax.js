0c33840f5fab15a8c350e10560d8d466b1120a7b
Thiago Lopes Costa <lliw.lopes@gmail.com>
2021-10-07 11:41:22 -0400

    Sub Reboot 01

--- teste_ajax.js
+++ teste_ajax.js
@@ -1,5 +1,14 @@
-var data = new FormData();
-data.append("data" , "Testando");
-var xhr = new XMLHttpRequest();
-xhr.open( 'post', 'teste_ajax.php', true );
-xhr.send(data);
+(function () {
+    
+    window.arquivo = function() {
+        console.log("Teste AJAX");
+        var data = new FormData();
+        data.append("data" , "Testando");
+        var xhr = new XMLHttpRequest();
+        xhr.open( 'post', 'teste_ajax.php', true );
+        xhr.send(data);
+    }
+    
+})();
+
+
