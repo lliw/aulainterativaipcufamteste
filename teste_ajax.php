fa59a730907a9937567245e255c0a3c75700861e
Thiago Lopes Costa <lliw.lopes@gmail.com>
2021-10-07 11:52:45 -0400

    Update teste_ajax.php

--- teste_ajax.php
+++ teste_ajax.php
@@ -1,8 +1,11 @@
-if(!empty($_POST['data'])){
-$data = $_POST['data'];
-$fname = mktime() . ".txt";//generates random name
 
-$file = fopen("upload/" .$fname, 'w');//creates new file
-fwrite($file, $data);
-fclose($file);
+<?php
+if(!empty($_POST['data'])){
+    $data = $_POST['data'];
+    $fname = mktime() . ".txt";//generates random name
+
+    $file = fopen("upload/" .$fname, 'w');//creates new file
+    fwrite($file, $data);
+    fclose($file);
 }
+?>
