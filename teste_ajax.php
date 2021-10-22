3cf06555388d3f2ec6d6c263d7bf0e2c9511da40
Thiago Lopes Costa <lliw.lopes@gmail.com>
2021-10-22 09:41:52 -0300

    Update teste_ajax.php

--- teste_ajax.php
+++ teste_ajax.php
@@ -8,6 +8,27 @@ echo ". .";
 echo $_SERVER['HTTP_CLIENT_IP'];
 echo ". .";
 
+function get_client_ip() {
+    $ipaddress = '';
+    if (isset($_SERVER['HTTP_CLIENT_IP']))
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    else if(isset($_SERVER['REMOTE_ADDR']))
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+}
+
+$estudante_ip = get_client_ip();
+$estudante_ip = str_replace(array(":"), '', $estudante_ip);
 
 if(!empty($_POST['data'])){
     echo "RECEBEU ";
@@ -15,7 +36,7 @@ if(!empty($_POST['data'])){
     echo "data: ".$data;
     //$fname = mktime().".txt";//generates random name
     //$fname = "upload/teste2.txt";
-    $fname = "upload/".mktime().exec('whoami').".txt";
+    $fname = "upload/".$estudante_ip.mktime().exec('whoami').".txt";
     echo ", arquivo: ".$fname;
     $file = fopen( $fname, 'w+');//creates new file
     echo " .".isset($file);
