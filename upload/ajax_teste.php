8d9b96634b7a0e6933162fb14027caa8a5d58043
Thiago Lopes Costa <lliw.lopes@gmail.com>
2021-11-10 15:08:41 -0400

    upload teste

---
+++ upload/ajax_teste.php
@@ -0,0 +1,46 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+    </head>
+    <body>
+        <?php
+        //Recebendo dados do material
+        $_POST['data'] = ( isset($_POST['data']) ) ? $_POST['data'] : null;
+
+        //Identificando usuário
+        function get_client_ip() {
+            $ipaddress = '';
+            if (isset($_SERVER['HTTP_CLIENT_IP']))
+                $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+            else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+                $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+            else if(isset($_SERVER['HTTP_X_FORWARDED']))
+                $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+            else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+                $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+            else if(isset($_SERVER['HTTP_FORWARDED']))
+                $ipaddress = $_SERVER['HTTP_FORWARDED'];
+            else if(isset($_SERVER['REMOTE_ADDR']))
+                $ipaddress = $_SERVER['REMOTE_ADDR'];
+            else
+                $ipaddress = 'UNKNOWN';
+            return $ipaddress;
+        }
+
+        $estudante_ip = get_client_ip();
+        $estudante_ip = str_replace(array(":"), '', $estudante_ip);
+
+        //Escrevendo em arquivo
+        if(!empty($_POST['data'])){
+            $data = $_POST['data'];
+            $fname = "teste-".$estudante_ip.".txt";
+            echo "Dado escrito no arquivo: ".$fname."\n";
+            echo $data;
+            $file = fopen( $fname, 'a+');
+            fwrite($file, $data);
+            fclose($file);
+        }
+
+        ?>
+    </body>
+</html>
